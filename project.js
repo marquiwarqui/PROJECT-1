6a66521ac1c4dbfbf008518c1d0e2a79b575444a
andresg6 <ajcapanzana@gmail.com>
2018-09-18 21:28:30 -0700

    fixedeffingmap

---
+++ project.js
@@ -0,0 +1,9 @@
+
+///****Below is the API for Google Maps ****/
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
